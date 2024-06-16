d2b9d7315fc5a4f6dd347224fb2605bbe4ce836a
farazpatwegar <farazpatwegar@gmail.com>
2024-06-16 23:33:52 +0530

    adding files

---
+++ HollowDiamond.java
@@ -0,0 +1,38 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class HollowDiamond {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i,j,no;
+		System.out.println("enter size of Diamond");
+		no=sc.nextInt();
+		for(i=1;i<=no;i++)
+		{
+			for(j=1;j<=no*2-1;j++)
+			{
+				if(j>no+1-i && j<no-1+i)
+					System.out.print("   ");
+				else
+					System.out.print(" * ");
+			}
+			System.out.println();
+		}
+		for(i=1;i<=no;i++)
+		{
+			for(j=1;j<=no*2-1;j++)
+			{
+				if(j<no*2-i && j>i)
+					System.out.print("   ");
+				else
+					System.out.print(" * ");
+			}
+			System.out.println();
+
+	}
+	}
+}
+
+
