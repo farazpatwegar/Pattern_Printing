d71560bd2fd81858cfeba1898765bf652347b88d
farazpatwegar <farazpatwegar@gmail.com>
2024-06-21 22:15:39 +0530

    adding files

---
+++ Triangle7.java
@@ -0,0 +1,24 @@
+package array;
+import java.util.Scanner;
+
+public class Triangle7 
+{
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int i,j,n;
+		System.out.println("Enter size of triangle:");
+		n=sc.nextInt();
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n;j++)
+			{
+				if(i==1||i==j||(j==n&&(i>=1&&i<=n)))
+					System.out.print(" * ");
+				else
+					System.out.print("   ");
+			}
+			System.out.println();
+		}
+	}
+}
